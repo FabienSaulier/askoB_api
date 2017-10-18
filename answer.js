fd192e8dfe7af844648bcccfdc57aadc22ab78c1
Fabien <fsaulier@gmail.com>
2017-10-18 10:14:12 +0000

    webhook

--- answer.js
+++ answer.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose');
 const mongooseStringQuery = require('mongoose-string-query');
 const timestamps = require('mongoose-timestamp');
 
+const SonSchema = new mongoose.Schema({ name: String, code: String });
+
 const AnswerSchema = new mongoose.Schema(
 	{
-		label: {
+	    
+	    code: {
 			type: String
 		},
-		text: {
+		name: {
 			type: String
 		},
+	    text: {
+			type: String
+		},
+	    sons: {
+			type: [SonSchema]
+		}
 	}
 );
 
--- app.js
+++ app.js
@@ -24,6 +24,8 @@ function respond(req, res, next) {
 
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.queryParser());
+
 
 server.get('/',  function(req, res, next) {    
     console.log(req.url);
@@ -42,18 +44,35 @@ server.get('/themes',  function(req, res, next) {
     next();
 });
 
+server.get('/webhook', function(req, res) {
+    
+    console.log(req);
+    
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "123456789") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 
-server.post('/answer',  function(req, res, next) {    
-    console.log(req.body);
+
+server.post('/answer',  function(req, res, next) {
+    
+    console.log("body: ",req.body);
     
     res.send(200);
     
+
     
     /* APPEL A FACEBOOk ! */
         httpRequest.post(
         {
-            'uri': 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHkcVMf1PgBAIJXgBFIOsGtcZA9ZBf0s3WXVjRIZB70Re1ZBarcleBHpyBS2mtCOmU0NVQ4gZCqgKBzFD8iRcpkWAPyLHpZBEGqDCoPNG2gevusPBc2ho1dlqsQnmMqAeh1lBA4ZAU1wbWlrtQxlZCaHtXrGHMbkhoBZA6DLjNLoCQZDZD',
-            'json': '{ "recipient":{ "id":"1564151846940529"  }, "message":{ "text":"hello,sdfdsfdsfsdf world!"}}'
+            url: 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHkcVMf1PgBAIJXgBFIOsGtcZA9ZBf0s3WXVjRIZB70Re1ZBarcleBHpyBS2mtCOmU0NVQ4gZCqgKBzFD8iRcpkWAPyLHpZBEGqDCoPNG2gevusPBc2ho1dlqsQnmMqAeh1lBA4ZAU1wbWlrtQxlZCaHtXrGHMbkhoBZA6DLjNLoCQZDZD',
+            json: true,
+            body:toto
         }, 
         function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
@@ -65,6 +84,51 @@ server.post('/answer',  function(req, res, next) {
     next();
 });
 
+
+server.get('/answer/:code_answer', function(req, res, next) {
+    
+    
+    const query = Answers.findOne({'code':req.params.code_answer});
+    
+    query.then(function(answer){
+        
+        
+        var fbResponse = {
+            "recipient":{ "id":"1564151846940529"  }, 
+            "message":{
+                "text":answer.text,
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Red",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                  }
+                ]},
+        };
+        
+
+        httpRequest.post(
+        {
+            url: 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHkcVMf1PgBAIJXgBFIOsGtcZA9ZBf0s3WXVjRIZB70Re1ZBarcleBHpyBS2mtCOmU0NVQ4gZCqgKBzFD8iRcpkWAPyLHpZBEGqDCoPNG2gevusPBc2ho1dlqsQnmMqAeh1lBA4ZAU1wbWlrtQxlZCaHtXrGHMbkhoBZA6DLjNLoCQZDZD',
+            json: true,
+            body:fbResponse
+        }, 
+        function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        });
+        
+
+        
+    })
+    
+    console.log(req.params);
+    console.log("coucou, code is : ",req.params.code_answer);
+    res.send(200);
+    
+    next();
+});
 
 
 server.listen(process.env.PORT || 3000, function() {
