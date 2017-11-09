0c3363f53222a98d7f9dbc7118ecd7f84123a2cd
Fabien Saulier <fsaulier@gmail.com>
2017-11-09 01:29:24 +0100

    conf

---
+++ config/config.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+module.exports = {
+	name: 'API',
+	env: process.env.NODE_ENV || 'development',
+	port: process.env.PORT || 3000,
+
+	db: {
+		uri: 'mongodb://admin:admin@ds113435.mlab.com:13435/heroku_wtzqjsm9'
+	},
+	ACCESS_TOKEN : process.env.ACCESS_TOKEN,
+};
