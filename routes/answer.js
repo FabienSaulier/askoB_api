e2e67cf91c6faa3f7b43065eb951f49144679f0a
Fabien Saulier <fsaulier@gmail.com>
2017-11-23 19:04:18 +0100

    create/update answer api

--- routes/answer.js
+++ routes/answer.js
@@ -32,6 +32,33 @@ export default(server) => {
   });
 
 
+  // Update and Create an asnwer.
+  server.put('/answer/', function(req, res, next){
+    const inputAnswer = req.body;
+    if(inputAnswer._id){
+      Answers.update({_id:inputAnswer._id}, inputAnswer, {runValidators: true}, function(err, answer){
+        if(err){
+          const e = flattenMongooseValidationError(err, ' - ');
+          res.send(400, {'errorMsg': e});
+          return next();
+        } else{
+          res.send(200);
+        }
+      })
+    }else{
+      Answers.create(inputAnswer, function(err){
+        if(err){
+          const e = flattenMongooseValidationError(err, ' - ');
+          res.send(400, {'errorMsg': e});
+          return next();
+        }else{
+          res.send(200);
+          return next();
+        }
+      })
+    }
+  });
+
 
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-import lapin from './lapin';
 import species from './species';
 import answer from './answer';
 import webhook from './webhook';
 
 // combine routes
 export default(server) => {
-  lapin(server);
   species(server);
   answer(server);
   webhook(server);
