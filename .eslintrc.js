982a4d17f18fffed758dc95d76bec7b492e75129
Fabien Saulier <fsaulier@gmail.com>
2018-01-09 17:56:33 +0100

    install eslint airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+  extends: 'airbnb-base',
+  rules: {
+    semi: 'off',
+    'linebreak-style': ['error', 'windows'],
+  },
 };
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import restify from 'restify'
 import routes from './routes'
 import logger from './lib/logger'
-import db from './model/db.js'
 import cors from './lib/cors'
 
-var server = restify.createServer()
+const server = restify.createServer()
 server.use(restify.plugins.bodyParser())
 server.use(restify.plugins.queryParser())
 server.pre(cors.preflight)
 server.use(cors.actual)
 
-server.listen(process.env.PORT || 3000, function() {
+server.listen(process.env.PORT || 3000, () => {
   logger.info('Server is listening on port 3000');
 });
 
