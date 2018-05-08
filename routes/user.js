7fbb2e9861b0a409775ee08700d49688d326252a
Fabien Saulier <fsaulier@gmail.com>
2018-05-08 07:08:28 +0200

    users search

--- routes/user.js
+++ routes/user.js
@@ -13,4 +13,21 @@ export default(server) => {
     res.send(200, users)
   })
 
+  /**
+  * get all the messages
+  * */
+  server.get('/user/search/', async (req, res) => {
+    console.log(req.query)
+    const users = await Users.find({
+      "createdAt": {
+        $gte: new Date(req.query.createdAtBegin),
+        $lte: new Date(req.query.createdAtEnd),
+        },
+      "question_species" : {
+        $in: req.query.species
+      }
+    }).exec()
+    res.send(200, users)
+  })
+
 }
